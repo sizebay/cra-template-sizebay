3a9f472c594c4628212cbc37cf96ba0332f9e251
Luiz Nickel <luizaugustonickel@gmail.com>
2021-07-05 22:49:10 -0300

    fix: file name typo

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import logo from './logo.svg'
 
-import { Container, Header, Logo } from './stlyes'
+import { Container, Header, Logo } from './styles'
 
 function App() {
   return (
